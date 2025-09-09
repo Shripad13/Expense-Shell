e2b5ffc837dca064ddd133c55d225dae9afb3562
Shripad Maddi <stmaddi@outlook.com>
2025-09-09 19:29:48 +0530

    pushing frontned.sh file

---
+++ frontend.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+COMPONENT="frontend"
+LOG_FILE="/tmp/frontend.log"
+
+ID=$(id -u)
+
+if [($ID) -ne 0]; then
+  echo -e "\e[33m You should be running this script as root or sudo privileges \n\t Usage: sudo $0 \e[0m"
+  exit 1
+fi
+
+echo -e "\e[33m Installing Nginx \e[0m"
+dnf install nginx -y                            &>> $LOG_FILE
+
+echo "Copying the configuration file"
+cp -prf expense.conf /etc/nginx/default.d/      &>> $LOG_FILE 
+
+echo "Downloading the $COMPONENT code"
+curl -o /tmp/$COMPONENT.zip https://expense-web-app.s3.amazonaws.com/$COMPONENT.zip   &>> $LOG_FILE
+
+echo "Extracting the $COMPONENT Code"
+cd /usr/share/nginx/html                        &>> $LOG_FILE
+
+echo "Removing the old default code if any"
+rm -rf /usr/share/nginx/html/*                  &>> $LOG_FILE
+
+echo "Unzipping the downloaded code"
+unzip /tmp/$COMPONENT.zip                         &>> $LOG_FILE
+
+echo "Starting the $COMPONENT service"
+systemctl enable nginx                          &>> $LOG_FILE
+systemctl restart nginx                         &>> $LOG_FILE
+systemctl status nginx                          &>> $LOG_FILE
+
+echo -e "\n\t $COMPONENT is up & running, Hit the Public IP on Browser"
