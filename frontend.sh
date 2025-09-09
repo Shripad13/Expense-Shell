fdfec367f2c102ab26a99dab46f67aa539eda401
Shripad Maddi <stmaddi@outlook.com>
2025-09-09 20:08:25 +0530

    adding updated file

--- frontend.sh
+++ frontend.sh
@@ -11,7 +11,7 @@ if [ "$ID" -ne 0 ]; then
 fi
 
 stat () {
-  if [ $1 -eq 0]; then
+  if [ $1 -eq 0] ; then
     echo -e "\e[32m Success \e[0m"
   else
     echo -e "\e[31m Failure \e[0m"
@@ -22,36 +22,36 @@ stat () {
 
 echo -e "\e[33m Installing Nginx \e[0m"
 dnf install nginx -y                            &>> $LOG_FILE
-stat ()
+stat $?
 
-echo -e "copy"
+echo -e "\e[33m  Copying  the configuration file \e[0m"
 cp -prf expense.conf /etc/nginx/default.d/      &>> $LOG_FILE 
-stat ()
+stat $?
 
 echo "Downloading the $COMPONENT code"
 curl -o /tmp/$COMPONENT.zip https://expense-web-app.s3.amazonaws.com/$COMPONENT.zip   &>> $LOG_FILE
-stat ()
+stat $?
 
 echo "Extracting the $COMPONENT Code"
 cd /usr/share/nginx/html                        &>> $LOG_FILE
-stat ()
+stat $?
 
 
 echo "Removing the old default code if any"
 rm -rf /usr/share/nginx/html/*                  &>> $LOG_FILE
-stat ()
+stat $?
 
 
 echo "Unzipping the downloaded code"
 unzip /tmp/$COMPONENT.zip                         &>> $LOG_FILE
-stat ()
+stat $?
 
 
 echo "Starting the $COMPONENT service"
 systemctl enable nginx                          &>> $LOG_FILE
 systemctl restart nginx                         &>> $LOG_FILE
 systemctl status nginx                          &>> $LOG_FILE
-stat ()
+stat $?
 
 
 echo -e "\n\t $COMPONENT is up & running, Hit the Public IP on Browser"
