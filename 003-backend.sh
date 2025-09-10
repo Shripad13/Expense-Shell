ebee7b29823edcd77cc7c53f8d409dd7ca56a342
Shripad Maddi <stmaddi@outlook.com>
2025-09-10 19:19:52 +0530

    adding updated file

--- 003-backend.sh
+++ 003-backend.sh
@@ -2,6 +2,10 @@
 
 source common.sh
 
+LOG_FILE=/tmp/backend.log
+COMPONENT=backend
+
+
 COLOR Listing the Modules
 dnf module list                                             &>> $LOG_FILE
 stat $?
@@ -18,12 +22,12 @@ COLOR Installing the nodejs 20
 dnf install nodejs -y                                        &>> $LOG_FILE
 stat $?
 
-COLOR Copying the backend service file
+COLOR Copying the $COMPONENT service file
 cp -prf backend.service /etc/systemd/system/                 &>> $LOG_FILE
 stat $?
 
-COLOR Downloading the backend code
-curl -o /tmp/backend.zip https://expense-web-app.s3.amazonaws.com/backend.zip     &>> $LOG_FILE
+COLOR Downloading the $COMPONENT code
+curl -o /tmp/$COMPONENT.zip https://expense-web-app.s3.amazonaws.com/$COMPONENT.zip     &>> $LOG_FILE
 stat $?
 
 
@@ -40,8 +44,8 @@ COLOR Changing the directory
 cd /app                                                       &>> $LOG_FILE
 stat $?
 
-COLOR Unzipping the backend code
-unzip /tmp/backend.zip                                         &>> $LOG_FILE        
+COLOR Unzipping the $COMPONENT code
+unzip /tmp/$COMPONENT.zip                                         &>> $LOG_FILE        
 stat $?
 
 COLOR Installing the npm package manager
@@ -58,14 +62,14 @@ dnf install mysql-server -y                                      &>> $LOG_FILE
 stat $?
 
 COLOR Injecting the schema
-mysql -h 172.31.46.19 -uroot -pExpenseApp@1 < /app/schema/backend.sql      &>> $LOG_FILE
+mysql -h 172.31.46.19 -uroot -pExpenseApp@1 < /app/schema/$COMPONENT.sql      &>> $LOG_FILE
 stat $?
 
-COLOR Starting the backend service
+COLOR Starting the $COMPONENT service
 systemctl daemon-reload                                             &>> $LOG_FILE
-systemctl enable backend                                            &>> $LOG_FILE
-systemctl restart backend                                           &>> $LOG_FILE
+systemctl enable $COMPONENT                                            &>> $LOG_FILE
+systemctl restart $COMPONENT                                           &>> $LOG_FILE
 stat $?
 
-echo -e "\e[35m \n\t Backend is up & Running \e[0m"
+echo -e "\e[35m \n\t $COMPONENT is up & Running \e[0m"
 
